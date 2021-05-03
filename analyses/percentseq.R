de802a16a77720629f6fadbbd82c22f722fd82c3
Alexa <71158788+pseudocrater@users.noreply.github.com>
2021-05-03 18:40:23 -0500

    New updated tree to reflect metadata

--- analyses/percentseq.R
+++ analyses/percentseq.R
@@ -8,8 +8,10 @@ library(treeio)
 
 ##percent sequence identity test
 setwd('/Users/alexa/Desktop/cactusvirusx/')
-al.f <- '/Users/alexa/Desktop/cactusvirusx/data/genbank_kr_consensus_extraction_al_trimmed.fasta'
+al.fCP <- '/Users/alexa/Desktop/cactusvirusx/data/data_v2/5_CP_trans_aln.fasta'
 
-aligned.cvx.phydat <- read.phyDat(al.f, format='fasta', type='DNA')
+aligned.cvx.phydat <- read.phyDat(al.fCP, format='fasta', type='DNA')
 aligned.cvx.bin <- as.DNAbin(aligned.cvx.phydat)
-dist.matrix <- dist.dna(aligned.cvx.bin, model = "raw", as.matrix=TRUE)
+dist.matrixCP <- dist.dna(aligned.cvx.bin, model = "raw", as.matrix=TRUE)
+write.csv(dist.matrixCP, file="distmatrixCP.csv")
+
