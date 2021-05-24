e55b57d804fb6118713040f924951587c4132d82
Alexa <71158788+pseudocrater@users.noreply.github.com>
2021-05-24 18:48:26 -0500

    Add KR naming system for fasta files to renaming function

--- analyses/percentseq.R
+++ analyses/percentseq.R
@@ -1,4 +1,4 @@
-#Install libraries
+#Load libraries
 library(ggtree) # tree plotting
 library(phangorn)
 library(phytools)
@@ -9,43 +9,40 @@ library(treeio)
 
 ##Set up paths for file access later
 setwd('/Users/alexa/Desktop/cactusvirusx/')
-accesslist <- c("/Users/alexa/Desktop/cactusvirusx/data/data_v3/5_CP.fasta",
-                "/Users/alexa/Desktop/cactusvirusx/data/data_v3/4_TGB3.fasta",
-                "/Users/alexa/Desktop/cactusvirusx/data/data_v3/3_TGB2.fasta",
-                "/Users/alexa/Desktop/cactusvirusx/data/data_v3/2_TGB1.fasta",
-                "/Users/alexa/Desktop/cactusvirusx/data/data_v3/1_RdRp.fasta",
+accesslist <- c(
+  "/Users/alexa/Desktop/cactusvirusx/data/data_v3/5_CP_nostop.fasta",
+  "/Users/alexa/Desktop/cactusvirusx/data/data_v3/4_TGB3_nostop.fasta",
+  "/Users/alexa/Desktop/cactusvirusx/data/data_v3/3_TGB2_nostop.fasta",
+  "/Users/alexa/Desktop/cactusvirusx/data/data_v3/2_TGB1_nostop.fasta",
+  "/Users/alexa/Desktop/cactusvirusx/data/data_v3/1_RdRp_nostop.fasta",
                 "/Users/alexa/Desktop/cactusvirusx/data/data_v3/0_full-aln.fasta")
-al.fCP <- '/Users/alexa/Desktop/cactusvirusx/data/data_v3/5_CP.fasta'
 
 
 #Renaming fasta function - still needs work
-cvxrename <- function(filepath, metadatapath) {
-  md <- read.csv(metadatapath, stringsAsFactors = FALSE)
+
+md <- read.csv(metadatapath, stringsAsFactors = FALSE)
+
+cvxrename = function(filepath) {
   tmpfile <- readChar(filepath,999999)
   for (i in (1:length(md$Name))){
-    #print(i)
-    print (md$Name[i])
-    print (md$Organism[i])
     #gsub(pattern, replacement, file)
-    
-    tmpfile <<-  gsub(md$Name[i], md$Organism[i], tmpfile, fixed = T)
-    
+    tmpfile <- gsub(md$Name[i], md$Organism[i], tmpfile, fixed = T)
+    write(tmpfile, file = "name.fasta")
   }
-  write(tmpfile, file = "Test1.fasta")
- 
 }
 
-
-
-mdpath <- '/Users/alexa/Desktop/cactusvirusx/data/data_v1/cvx_hostdata_v3.csv'
-testfasta <- accesslist[1]
-cvxrename(testfasta, mdpath)
+cvxrename(accesslist[1])
 
 #Percent seq .csv production, given .fasta file.
-choose.node <- function(filepath) {
+perc.seq <- function(filepath, name) {
+  aligned.cvx.phydat <- read.phyDat(filepath, format='fasta', type='DNA')
+aligned.cvx.bin <- as.DNAbin(aligned.cvx.phydat)
+dist.matrix <- dist.dna(aligned.cvx.bin, model = "raw", as.matrix=TRUE)
+
+  write.csv(dist.matrix, file=name)
+print(filepath)
+}
+
+for (i in (1:length(c("5_CP_perseq.csv", "4_TGB3_perseq.csv", "3_TGB2_perseq.csv", "2_TGB1_perseq.csv","1_RdRp_perseq.csv")))) {
   
 }
-#aligned.cvx.phydat <- read.phyDat(al.fCP, format='fasta', type='DNA')
-#aligned.cvx.bin <- as.DNAbin(aligned.cvx.phydat)
-#dist.matrixCP <- dist.dna(aligned.cvx.bin, model = "raw", as.matrix=TRUE)
-#write.csv(dist.matrixCP, file="distmatrixCP.csv")
