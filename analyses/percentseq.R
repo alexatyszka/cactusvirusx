9c3e2fbfaae951e14fae335f28376e05a321a640
Alexa <71158788+pseudocrater@users.noreply.github.com>
2021-05-25 10:11:33 -0500

    Updated named .fasta nostop files added

--- analyses/percentseq.R
+++ analyses/percentseq.R
@@ -11,28 +11,31 @@ library(treeio)
 setwd('/Users/alexa/Desktop/cactusvirusx/')
 accesslist <- c(
   "/Users/alexa/Desktop/cactusvirusx/data/data_v3/5_CP_nostop.fasta",
-  "/Users/alexa/Desktop/cactusvirusx/data/data_v3/4_TGB3_nostop.fasta",
+  "/Users/alexa/Desktop/cactusvirusx/data/data_v3/4_TGB3.fasta",
   "/Users/alexa/Desktop/cactusvirusx/data/data_v3/3_TGB2_nostop.fasta",
-  "/Users/alexa/Desktop/cactusvirusx/data/data_v3/2_TGB1_nostop.fasta",
+  "/Users/alexa/Desktop/cactusvirusx/data/data_v3/2_TGB1.fasta",
   "/Users/alexa/Desktop/cactusvirusx/data/data_v3/1_RdRp_nostop.fasta",
                 "/Users/alexa/Desktop/cactusvirusx/data/data_v3/0_full-aln.fasta")
 
 
 #Renaming fasta function - still needs work
-
+metadatapath <- "/Users/alexa/Desktop/cactusvirusx/data/data_v1/cvx_hostdata_v3.csv"
 md <- read.csv(metadatapath, stringsAsFactors = FALSE)
 
-cvxrename = function(filepath) {
+cvxrename = function(filepath, name) {
   tmpfile <- readChar(filepath,999999)
   for (i in (1:length(md$Name))){
     #gsub(pattern, replacement, file)
     tmpfile <- gsub(md$Name[i], md$Organism[i], tmpfile, fixed = T)
-    write(tmpfile, file = "name.fasta")
+    write(tmpfile, file = name)
   }
 }
 
-cvxrename(accesslist[1])
 
+names <-c("5_CP_nostop_names.fasta", "4_TGB3_names.fasta", "3_TGB2_nostop_names.fasta", "2_TGB1_names.fasta","1_RdRp_nostop_names.fasta", "0_full-aln_names.fasta")
+for (i in (1:length(names))) {
+  cvxrename(accesslist[i], names[i])
+}
 #Percent seq .csv production, given .fasta file.
 perc.seq <- function(filepath, name) {
   aligned.cvx.phydat <- read.phyDat(filepath, format='fasta', type='DNA')
