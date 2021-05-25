035fd2bd7ef4389e5c0f1a28624a7080004edba7
Alexa <71158788+pseudocrater@users.noreply.github.com>
2021-05-25 17:39:21 -0500

    Adding iqtree for genes

--- analyses/percentseq.R
+++ analyses/percentseq.R
@@ -27,11 +27,13 @@ cvxrename = function(filepath, name) {
   for (i in (1:length(md$Name))){
     #gsub(pattern, replacement, file)
     tmpfile <- gsub(md$Name[i], md$Organism[i], tmpfile, fixed = T)
-    write(tmpfile, file = name)
   }
+  tmpfile <- gsub(" ", "_", tmpfile, fixed = T)
+  tmpfile <- gsub(":", "_", tmpfile, fixed = T)
+  write(tmpfile, file = name)
 }
 
-
+#Run this renaming for all ORFs and full genome.
 names <-c("5_CP_nostop_names.fasta", "4_TGB3_names.fasta", "3_TGB2_nostop_names.fasta", "2_TGB1_names.fasta","1_RdRp_nostop_names.fasta", "0_full-aln_names.fasta")
 for (i in (1:length(names))) {
   cvxrename(accesslist[i], names[i])
@@ -45,7 +47,8 @@ dist.matrix <- dist.dna(aligned.cvx.bin, model = "raw", as.matrix=TRUE)
   write.csv(dist.matrix, file=name)
 print(filepath)
 }
-
+#Run this for all ORFS and full genome.
 for (i in (1:length(c("5_CP_perseq.csv", "4_TGB3_perseq.csv", "3_TGB2_perseq.csv", "2_TGB1_perseq.csv","1_RdRp_perseq.csv")))) {
   
 }
+
