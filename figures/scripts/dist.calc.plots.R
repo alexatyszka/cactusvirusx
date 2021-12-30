a1c853001b99299d63e3a869f54967351f9408bb
Alexa <71158788+pseudocrater@users.noreply.github.com>
2021-12-30 12:19:16 -0600

    Addition of some different visualizations of sequence distance for clades.

---
+++ figures/scripts/dist.calc.plots.R
@@ -0,0 +1,24 @@
+#at, 12-20-21, for cvx
+##this file should produce a csv file with the necessary information for plotting
+library(ggtree)
+library(phangorn)
+library(phytools)
+library(ape)
+library(DECIPHER)
+library(treeio)
+library(dplyr)
+library(ggplot2)
+full.loc <- "../../data/distdna/full_seqs_dist_clades.csv"
+  rdrp.loc <- "../../data/distdna/rdrp_dist_clades.csv" 
+cp.loc <- "../../data/distdna/cp_dist_clades.csv"
+full.data <- read.csv(full.loc, stringsAsFactors = TRUE, header = TRUE)
+ggplot(full.data, aes(y=Dist, x=Group, fill=Group))+
+  scale_color_manual(name="Group", values=c("#00A9E7", "#00B9A5", "#3FB13A", "#B39B00", "#E67E67", "#EC6DC4", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#a6cee3","#1f78b4","#b2df8a","#33a02c"))+
+  geom_point(aes(color=Group), shape=95, alpha=0.1, size=22)+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90, hjust=1),
+        panel.grid.major.x  = element_blank(),
+        )+
+  labs(y="Calculated Sequence Distances", x="")
+filename = "dist_bars_graph.pdf"
+ggsave(filename, height = 20, units = "cm")
--- figures/scripts/distance.matrix.calcs.R
+++ figures/scripts/distance.matrix.calcs.R
@@ -1,79 +1,79 @@
 #at, 12-20-21, for cvx
+##this file should produce a csv file with the necessary information for plotting
 library(ggtree)
 library(phangorn)
 library(phytools)
 library(ape)
 library(DECIPHER)
 library(treeio)
+library(dplyr)
 full.loc <- "../../data/distdna/0full.csv"
 rdrp.loc <- "../../data/distdna/1rdrp.csv" 
 tgb1.loc <- "../../data/distdna/2tgb1.csv"
 tgb2.loc <- "../../data/distdna/3tgb2.csv"
 tgb3.loc <- "../../data/distdna/4tgb3.csv"
 cp.loc <- "../../data/distdna/5cp.csv"
-#generate graph like sdt does of frequencies
-#separate by clade
-#
-#
-#
-cladesearch <- function (cladename){
-  metarowname <- c()
+#load csv files 
+full <- read.csv(full.loc, header=FALSE)
+full.names <- full$V1
+#load metadata file
+md <- read.csv('../../data/name_key.csv', stringsAsFactors = FALSE)
+#remove lower diagonal of matrix
+full[lower.tri(full, diag = TRUE)] <- NA
+#add back names
+full$V1 <- full.names
+joined <- left_join(full, md, by = c("V1" = "Name_updated"))
+clades.list <- unique(md$group)[2:7]
+dist.csv <- data.frame(matrix(ncol=2, nrow=1))
+colnames(dist.csv) <- c("Group", "Dist")
+for (i in 1:length(clades.list)){
+  cladename <- clades.list[i]
+  selected.seqs <- c()
   #fetch organism names from metadata file matching Clade column with clade name
-  metarowname <- md$Organism[grep(cladename, md$Clade)]
-  #change metadata organism names to match csv format
-  metarowname <- gsub(" ", "_", metarowname)
-  metarowname <- gsub(":", "", metarowname)
-  
-  #Note: Schlumbergera samples from gene specific ORF fastas have been renamed, which has been reflected in the metadata csv file.
-  
-  
+  selected.seqs <- joined$V1[grep(cladename, joined$group)]
   #Fetch the associated csv row/column number for items within list of organism names
-  fullnum <<- c()
-  for(i in 1:length(metarowname)){
-    fullnum[i] <<- grep(metarowname[i], rownames(full))
-    #print(metarowname[i])
-  }
-  if (anyNA(fullnum)){
-    print("Warning! One or more full.csv values is NA. Please check naming in all files.")
+  fullnum <- c()
+  for(i in 1:length(selected.seqs)){
+    print(i)
+    fullnum[i] <- grep(selected.seqs[i], full$V1)
+    #print(selected.seqs[i])
   }
   fullcomb <- combn(sort(unique(fullnum)), 2)
-  
-  cpnum <<- c()
-  for(i in 1:length(metarowname)){
-    cpnum[i] <<- grep(metarowname[i], rownames(cp))
-    #print(metarowname[i])
-    #print(cpnum[i])
-  }
-  if (anyNA(cpnum)){
-    print("Warning! One or more cp values is NA. Please check naming in all files.")
-  }
-  cpcomb <- combn(sort(unique(cpnum)), 2)
-  
-  #exceptions for missing rows/columns in rdrp file
-  searchrdrp <- c("KM288843","Cactus_virus_X_KM288847.1",
-                  "KM288842","KM288844","KM288845", 
-                  "Cactus_virus_X__KM288847.1")
-  if(any(metarowname %in%  searchrdrp)){
-    print("Note: removing values because sequences did not cover the rdrp gene. Sequence removed:")
-    print(metarowname[(metarowname %in%
-                         searchrdrp)])
-    metarowname <- metarowname[!(metarowname %in%
-                                   searchrdrp)]
-  }
-  rdrpnum <<- c()
-  for(i in 1:length(metarowname)){
-    rdrpnum[i] <<- grep(metarowname[i], rownames(rdrp))
-    #print(metarowname[i])
-    #print(rdrpnum[i])
-  }
-  if (anyNA(rdrpnum)){
-    print("Warning! One or more rdrp values is NA. Please check naming in all files.")
-    print(rdrpnum)
-  }
-  rdrpcomb <- combn(sort(unique(rdrpnum)), 2)
-  
-  #Next, use indices to grab maximum and average pairwise distances.
-  #full
+  # 
+  # cpnum <<- c()
+  # for(i in 1:length(selected.seqs)){
+  #   cpnum[i] <<- grep(selected.seqs[i], rownames(cp))
+  #   #print(selected.seqs[i])
+  #   #print(cpnum[i])
+  # }
+  # if (anyNA(cpnum)){
+  #   print("Warning! One or more cp values is NA. Please check naming in all files.")
+  # }
+  # cpcomb <- combn(sort(unique(cpnum)), 2)
+  # 
+  # #exceptions for missing rows/columns in rdrp file
+  # searchrdrp <- c("KM288843","Cactus_virus_X_KM288847.1",
+  #                 "KM288842","KM288844","KM288845", 
+  #                 "Cactus_virus_X__KM288847.1")
+  # if(any(selected.seqs %in%  searchrdrp)){
+  #   print("Note: removing values because sequences did not cover the rdrp gene. Sequence removed:")
+  #   print(selected.seqs[(selected.seqs %in%
+  #                        searchrdrp)])
+  #   selected.seqs <- selected.seqs[!(selected.seqs %in%
+  #                                  searchrdrp)]
+  # }
+  # rdrpnum <<- c()
+  # for(i in 1:length(selected.seqs)){
+  #   rdrpnum[i] <<- grep(selected.seqs[i], rownames(rdrp))
+  #   #print(selected.seqs[i])
+  #   #print(rdrpnum[i])
+  # }
+  # if (anyNA(rdrpnum)){
+  #   print("Warning! One or more rdrp values is NA. Please check naming in all files.")
+  #   print(rdrpnum)
+  # }
+  # rdrpcomb <- combn(sort(unique(rdrpnum)), 2)
+
   #populate vector...
   numtemp <- vector()
   for (i in 1:length(fullcomb[1,])){
@@ -83,47 +83,28 @@ cladesearch <- function (cladename){
   for (i in 1:length(fullcomb[1,])){
     numtemp[i] <- full[fullcomb[1,i],fullcomb[2,i]]
   }
-  #now calculate average and maximum values for these pairwise distances
-  fullmax <-c(max(numtemp))
-  fullavg <- c(mean(numtemp))
-  #repeat for the other genes
-  #rdrp
-  numtemp <- vector()
-  for (i in 1:length(rdrpcomb[1,])){
-    numtemp[i] <- i
-  }
-  for (i in 1:length(rdrpcomb[1,])){
-    numtemp[i] <- rdrp[rdrpcomb[1,i],rdrpcomb[2,i]]
-  }
-  rdrpmax <-c(max(numtemp))
-  rdrpavg <- c(mean(numtemp))
-  #cp
-  numtemp <- vector()
-  for (i in 1:length(cpcomb[1,])){
-    numtemp[i] <- i
-  }
-  for (i in 1:length(cpcomb[1,])){
-    numtemp[i] <- cp[cpcomb[1,i],cpcomb[2,i]]
-  }
-  cpmax <-c(max(numtemp))
-  cpavg <- c(mean(numtemp))
-  nums <- list("Max Values" = format(round(c(fullmax, rdrpmax, cpmax), 3), nsmall = 3), "Avg Values" = format(round(c(fullavg, rdrpavg, cpavg), 3), nsmall = 3) )
-  nums
+  numtemp
+df.to.add <- data.frame(rep(cladename, length(numtemp[numtemp!=0])), numtemp[numtemp!=0]) 
+colnames(df.to.add) <- c("Group", "Dist")
+dist.csv <<- bind_rows(dist.csv, df.to.add)
+ # #repeat for the other genes
+ #  #rdrp
+ #  numtemp <- vector()
+ #  for (i in 1:length(rdrpcomb[1,])){
+ #    numtemp[i] <- i
+ #  }
+ #  for (i in 1:length(rdrpcomb[1,])){
+ #    numtemp[i] <- rdrp[rdrpcomb[1,i],rdrpcomb[2,i]]
+ #  }
+ # 
+ #  #cp
+ #  numtemp <- vector()
+ #  for (i in 1:length(cpcomb[1,])){
+ #    numtemp[i] <- i
+ #  }
+ #  for (i in 1:length(cpcomb[1,])){
+ #    numtemp[i] <- cp[cpcomb[1,i],cpcomb[2,i]]
+ #  }
+ write.csv(dist.csv, file="dist_clades.csv")
 }
-
-
-#Fetch values for table.
-
-clades <- c("Cactus Virus X Variant A", " ", " ", "Cactus Virus X Variant B", " ", " ", "Zygocactus Virus X Variant B1", " ", " ", "Pitaya Virus X", " ", " ","Schlumbergera Virus X", " ", " ","Opuntia Virus X", " ", " ")
-lnames <- rep(c("Full sequence", "RdRp", "Coat Protein"),6)
-#matrix selection: matrix[rows, columns]
-lmax <- c(cva$`Max Values`[1:3], cvb$`Max Values`[1:3], zyvx$`Max Values`[1:3] ,pvx$`Max Values`[1:3], svx$`Max Values`[1:3], opvx$`Max Values`[1:3])
-lavg <- c(cva$`Avg Values`[1:3], cvb$`Avg Values`[1:3], zyvx$`Avg Values`[1:3], pvx$`Avg Values`[1:3], svx$`Avg Values`[1:3], opvx$`Avg Values`[1:3] ) 
-
-#Store values in table, then save table as .pdf file.
-
-
-sumstats <- cbind(clades, lnames, lmax, lavg)
-kbl(sumstats, booktabs = T, format="html", col.names = c("Species", "Open Reading Frame (ORF)", "Maximum Pairwise Distance", "Average Pairwise Distance"))  %>% kable_styling() %>% save_kable("test.pdf")
-
 
