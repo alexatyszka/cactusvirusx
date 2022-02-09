1d7eb0637291b90629bd0d78400fc8da872f1510
Alexa <71158788+pseudocrater@users.noreply.github.com>
2022-02-09 11:39:38 -0600

    Sandboxes supporting multiple user paths using save() and load() added
    Phylogeny_annotation.R is a cleaned version of full_and_gene_phylos.Rmd, intended for easier cross-user use. It contains code to individually or batch create .pdf file(s) for a list of phylogenies.

---
+++ figures/scripts/phylogeny_annotation.R
@@ -0,0 +1,110 @@
+#at, 2-9-22, for cvx
+#Notes:S4 objects and their slots can be accessed like this: "tree@example". 
+load("objects.for.phylo.RData")
+ls()
+#tree visualization:
+par(mar = c(0, 0, 0, 0))
+ggtree(cvx.tree.phylo.treedata.joined, ladderize=T, 
+           aes(color=formal_tax, family="Helvetica"), show.legend=FALSE)+
+#host tip labels:
+geom_tiplab(aes(label=host, subset = !is.na(host)), linetype="dotted", 
+            align=T, fontsize=3, na.rm=F, 
+            #use for all trees:
+            #offset=0.8, 
+            #use for just the full tree:
+            offset=0.3,
+            fontface=3)+
+#virus name tip labels:
+geom_tiplab(aes(), geom="label",fontsize=3, fill="white", offset=0.1,
+              family='Helvetica', align=F, label.size = 0)+
+#scale:
+geom_treescale(x=0.25, y=60, width=0.25, fontsize=8, linesize=1, offset=1, 
+               color='black', label='substitutions per site', offset.label=-1)+
+#node labels/bootstrap labels:
+geom_text2(aes(subset = !isTip , label=label), nudge_x = 0.025, size=3)+
+            xlim(0,4.5)+
+#newness asterisk labels:
+geom_tiplab(aes(label=new),size=10, color="black", family='Helvetica', align=T, 
+            na.rm=F, offset=0.26,vjust=0.8, linetype="blank", fontface=1)+
+#group labels:
+scale_color_manual( name="Group", values=c("gray50", 
+           "#0072B2",
+           "#3FB13A", 
+           #"#BE81EF",
+           "#B39B00", 
+           "#E67E67", 
+           "#EC6DC4", 
+           "grey") )+
+#theme specifics:
+theme(legend.position = c(0.1,0.9),
+          legend.key.size = unit(2, 'cm'),
+          legend.text = element_text(size = 20))
+
+#Save to pdf format if desired
+filename <- "full_seq_phylo_formal_tax.pdf"
+ggsave(filename,width = 70, height = 50, units = "cm")
+
+
+
+##batch creation code begins below
+#Load metadata if needed, handy function to be used within larger function
+reload.metadata <- function(){
+  host.info.details <<- read.csv(host.info.loc, stringsAsFactors = FALSE)
+  print("Metadata file loaded")
+  reload.metadata()
+  getwd()}
+#optional long, clunky function to systematically create phylogenies for each gene.
+gene.phylos <- function(namevector, locvector) {
+  reload.metadata()
+  for (i in 1:length(namevector)){
+    cvx.tree.phylo.treedata <- read.iqtree(locs[1])
+    cvx.tree.phylo.treedata <- read.iqtree(locvector[i])
+    treename <- namevector[i]
+    #  treetempphylo <- as.phylo(treetemp)
+    
+    print(namevector[i])
+    #join tree and metadata
+    cvx.tree.phylo.treedata@phylo <- ape::root(cvx.tree.phylo.treedata@phylo, outgroup=c("D29630.1"), resolve.root=TRUE)
+    cvx.tree.phylo.treedata
+    cvx.tree.phylo.treedata.joined <- as.treedata(dplyr::left_join(as_tibble(cvx.tree.phylo.treedata), as_tibble(host.info.details), by=c("label"= "Name_updated")))
+    cvx.tree.phylo.treedata.joined@phylo <- cvx.tree.phylo.treedata@phylo
+    par(mar = c(0, 0, 0, 0))
+    ggtree(cvx.tree.phylo.treedata.joined, ladderize=T, aes(color=formal_tax, family="Helvetica"), show.legend=FALSE)+
+      #host tip labels:
+      geom_tiplab(aes(label=host, subset = !is.na(host)), linetype="dotted", align=T, fontsize=3, na.rm=F, 
+                  #use for all trees:
+                  offset=0.8, 
+                  #use for just the full tree:
+                  #offset=0.3,
+                  fontface=3)+
+      
+      #virus name tip labels:
+      geom_tiplab(aes(), geom="label",fontsize=3, fill="white", offset=0.1,family='Helvetica', align=F, label.size = 0)+
+      
+      #scale
+      geom_treescale(x=0.25, y=60, width=0.25, fontsize=8, linesize=1, offset=1, color='black', label='substitutions per site', offset.label=-1)+
+      #node labels/bootstrap labels:
+      geom_text2(aes(subset = !isTip , label=label), nudge_x = 0.025, size=3)+
+      xlim(0,4.5)+
+      
+      #newness tip labels:
+      geom_tiplab(aes(label=new),size=10, color="black", family='Helvetica', align=T, na.rm=F, offset=0.26,vjust=0.8, linetype="blank", fontface=1)+
+      #group labels
+      scale_color_manual( name="Group", values=c("gray50", 
+                                                 "#0072B2",
+                                                 "#3FB13A", 
+                                                 #"#BE81EF",
+                                                 "#B39B00", 
+                                                 "#E67E67", 
+                                                 "#EC6DC4", 
+                                                 "grey") )+
+      theme(legend.position = c(0.1,0.9),
+            legend.key.size = unit(2, 'cm'),
+            legend.text = element_text(size = 20))
+    
+    #Save to pdf format
+    filename = paste(namevector[i],"_tr_formal_tax.pdf",sep="")
+    ggsave(filename,width = 70, height = 50, units = "cm")
+  }
+}
+gene.phylos(names,locs)
---
+++ figures/scripts/save_sandbox_bi.R
@@ -0,0 +1,38 @@
+#created by at, 2-9-22, for cvx
+#Sandbox for BI, saving all items needed for tree generation and experimentation
+#the objects created in this file are intended to be loaded and used in phylogeny_annotation.R
+#packages
+library(treeio)
+library(dplyr)
+library(tidytree)
+library(ggtree) 
+library(DECIPHER)
+library(phytools)
+library(ape)
+library(ggplot2)
+getwd()
+directory <- "examplepath/cactusvirusx/"
+setwd(dir = directory)
+host.info.loc <-'data/name_key.csv'
+host.info.details <- read.csv(host.info.loc, stringsAsFactors = FALSE)
+loc0 <- "data/iqtree_0_full-aln_names/0_full-aln_names.fasta.treefile"
+loc1 <- "data/iqtree_1_RdRp_names/1_RdRp_names.fasta.treefile"
+loc2 <- "data/iqtree_2_TGB1_names/2_TGB1_names.fasta.treefile"
+loc3 <- "data/iqtree_3_TGB2_names/3_TGB2_names.fasta.treefile"
+loc4 <- "data/iqtree_4_TGB3_names/4_TGB3_names.fasta.treefile"
+loc5 <- "data/iqtree_5_CP_names/5_CP_names.fasta.treefile"
+locs <- c(loc0, loc1, loc2, loc3, loc4, loc5)
+names <- c("full", "rdrp", "tgb1", "tgb2", "tgb3", "cp")
+#load trees
+cvx.tree.phylo.treedata <- read.iqtree(locs[1])
+#root tree, outgroup can be changed:
+cvx.tree.phylo.treedata@phylo <- ape::root(cvx.tree.phylo.treedata@phylo, outgroup=c("D29630.1"), resolve.root=TRUE)
+#is.rooted(cvx.tree.phylo.treedata)
+#use dplyr to join any host data
+cvx.tree.phylo.treedata.joined <- as.treedata(dplyr::left_join(as_tibble(cvx.tree.phylo.treedata), as_tibble(host.info.details), by=c("label"= "Name_updated")))
+#now replace the phylo S4 object of the joined tree with that of the original. 
+#for some reason, things break massively without this.
+cvx.tree.phylo.treedata.joined@phylo <- cvx.tree.phylo.treedata@phylo
+save(host.info.details, host.info.loc, locs, names, cvx.tree.phylo.treedata.joined, file = "objects.for.phylo.RData")
+save.image()
+
