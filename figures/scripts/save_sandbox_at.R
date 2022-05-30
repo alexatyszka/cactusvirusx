3ddd2960ec9118c1e0ee439b06befe7aa71982a2
Alexa <71158788+pseudocrater@users.noreply.github.com>
2022-05-29 23:28:34 -0400

    Addition of figures and Keynote presentation for Evolution 2022
    Circular tree and transparent versions of gene plots added

--- figures/scripts/save_sandbox_at.R
+++ figures/scripts/save_sandbox_at.R
@@ -9,6 +9,14 @@ library(DECIPHER)
 library(phytools)
 library(ape)
 library(ggplot2)
+library(ggtreeExtra)
+to.drop <- c("KU159093", "LC155795", "AY800279",
+             "KU697313", "KT717325",
+             "FJ822136", "GQ179646", "GQ179647",
+             "AY863024", "MH423501",
+             "MF978248","JX524226", "LC107515","KX196173",
+             "D29630.1", 
+             "LC107517")
 getwd()
 directory <- "Documents/GitHub/cactusvirusx/"
 setwd(dir = directory)
@@ -22,21 +30,19 @@ loc4 <- "data/iqtree-output/iqtree_4_TGB3_names/4_TGB3_names.fasta.treefile"
 loc5 <- "data/iqtree-output/iqtree_5_CP_names/5_CP_names.fasta.treefile"
 locs <- c(loc0, loc1, loc2, loc3, loc4, loc5)
 names <- c("full", "rdrp", "tgb1", "tgb2", "tgb3", "cp")
-#controversial <- read.csv(file = "data/controv.csv")
-#host.info.details <- left_join(host.info.details, controversial, by = c("Name_updated" = "X"))
-#write.csv(host.info.details, file="controv.details.csv")
 #load trees
 cvx.tree.phylo.treedata <- read.iqtree(locs[1])
 #root tree, outgroup can be changed:
-og <- MRCA(cvx.tree.phylo.treedata, c("FJ822136", "D29630.1"))
-cvx.tree.phylo.treedata@phylo <- ape::root(cvx.tree.phylo.treedata@phylo, node=og, resolve.root=TRUE)
-#is.rooted(cvx.tree.phylo.treedata)
+MRCA(cvx.tree.phylo.treedata, c("LC107517", "FJ822136"))
+cvx.tree.phylo.treedata@phylo <- ape::root(cvx.tree.phylo.treedata@phylo, outgroup=to.drop, resolve.root=TRUE)
+is.rooted(cvx.tree.phylo.treedata@phylo)
 #use dplyr to join any host data
 cvx.tree.phylo.treedata.joined <- as.treedata(dplyr::left_join(as_tibble(cvx.tree.phylo.treedata), as_tibble(host.info.details), by=c("label"= "Name_updated")))
 #now replace the phylo S4 object of the joined tree with that of the original. 
 #for some reason, things break massively without this.
 cvx.tree.phylo.treedata.joined@phylo <- cvx.tree.phylo.treedata@phylo
 cvx.tree.phylo.treedata.joined@data$new[is.na(cvx.tree.phylo.treedata.joined@data$new)] <- "FALSE"
+
 save(host.info.details, host.info.loc, locs, names, cvx.tree.phylo.treedata.joined, file = "objects.for.phylo.RData")
 save.image()
 
