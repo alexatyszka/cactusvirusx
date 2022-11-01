218c1fa6edfb45a0a75e033b73396158da48626c
Alexa <71158788+pseudocrater@users.noreply.github.com>
2022-11-01 06:15:30 -0500

    Updated phylogeny, discussion section, and addition of figures to manuscript

--- figures/scripts/02_phylogeny.R
+++ figures/scripts/02_phylogeny.R
@@ -8,7 +8,8 @@ library(DECIPHER)
 library(phytools)
 library(ape)
 library(ggplot2)
-host.info.details <- read.csv("figures/scripts/phylogeny_hosts.csv", stringsAsFactors = FALSE)
+library(ggnewscale)
+host.info.details <- read.csv("figures/scripts/02_phylogeny_hosts.csv", stringsAsFactors = FALSE)
 treedata <- read.iqtree(file = "02_analyses/02_iqtree/complete_gb_and_srr.r.aln.fasta.treefile")
 treedata@phylo <- reroot(treedata@phylo, interactive = TRUE)
 to_drop <- c("CYMRNA", "LC107517", "KU697313", "KU159093", "KT717325", "LC155795", "AY800279", "AY863024", "GQ179646", "GQ179647", "MH423501", "FJ822136", "LC107515", "JX524226", "KX196173", "MF978248")
@@ -30,8 +31,8 @@ labels=c("no", "yes"))+
   geom_tiplab(aes( label=host, color=new), align=T, linetype=NA, size=4, offset=0.4, hjust=0)+
   geom_treescale(x=0.1, y=70,width=0.10, fontsize=4, linesize=1, offset=2, color='black', label='\n substitutions per site', offset.label=2)+
   theme(legend.position="none")+
-  xlim(0,1.2)
-#geom_text(aes(label=node), hjust=-.3)
+  xlim(0,1.2)+
+  geom_text(aes(label=UFboot), hjust=-.3)
 p
 ggsave("tree_rect.pdf",width = 50, height = 50, units = "cm")
 
@@ -41,9 +42,18 @@ heatmapData <- as.data.frame(sapply(host.info.details, as.character))
 rn <- as.data.frame(heatmapData[2])
 rownames(heatmapData) <- rn$Name
 
-gheatmap(p, heatmapData[c(4,6,7)], colnames=F, 
-         hjust=0.3, offset=0.65, width=0.2)+
-  xlim(0,1.7)
+heatmapData[c(8)] <- rep(NA, 120)
+
+
+p1 <- gheatmap(p, heatmapData[c(6, 7, 8)], colnames=F, 
+         hjust=0.3, offset=0.65, width=0.2, colnames_angle=45)+
+  xlim(0,1.7)+
+  scale_fill_viridis_d(option="H", name="mPTP and bPTP delimitation")
+p2 <- p1 + new_scale_fill()
+gheatmap(p2, heatmapData[c(4)], offset=0.9, width=.1,
+         colnames=F,) +
+  scale_fill_viridis_d(option="C", name="Formal taxonomy")
+
 ggsave("tree_circular_delim_info.pdf",width = 50, height = 50, units = "cm")
 
 
---
+++ figures/scripts/04_heatmap.R
@@ -0,0 +1,65 @@
+#at, 11-21-21, for cvx
+library("ggplot2")
+library("pheatmap")
+library("dplyr")
+setwd("/Users/alexa/Documents/GitHub/cactusvirusx/")
+getwd()
+  dat <- read.csv('02_analyses/04_distdna/distance_fullseq_perc.csv', stringsAsFactors = FALSE)
+
+annot <-read.csv("figures/scripts/02_phylogeny_hosts.csv") 
+
+row.names(dat) <- dat$X
+dat <- subset(dat, select=-X)
+#copy lower triangle to upper triangle:
+#dat[upper.tri(dat)] <- t(dat)[upper.tri(t(dat))]
+#annotations:
+annotationcols <- subset(annot, select=c(Formal.taxon, Name))
+annotfilt <- subset(annotationcols, Formal.taxon %in% c("Cactus virus X", 
+                                                        "Mytcor virus 1",
+                                                        "Opuntia virus X",
+                                                        "Pitaya virus X",
+                                                        "Schlumbergera virus X",
+                                                        "Zygocactus virus X"))
+rownames(annotfilt) <- annotfilt$Name
+annotfilt <- subset(annotfilt, select=-Name)
+#flip dat
+#dat=dat[,order(ncol(dat):1)]
+
+par(mar=c(0,0,0,2))
+pheatmap::pheatmap(dat, na_col = "grey80",
+         cluster_rows=T, cluster_cols = T, 
+         annotation_col = annotfilt,
+         annotation_row = annotfilt,
+         annotation_names_col = T,
+         annotation_names_row = T,
+         treeheight_row = 40,
+         treeheight_col = 40,
+         border_color = "white",
+         main = "",
+         color = c(
+           "white",
+           "#ffffe5",
+           "#f7fcb9",
+           "#d9f0a3",
+           "#addd8e",
+           "#78c679",
+           "#41ab5d",
+           "#238554",
+           "black"),
+         breaks = c(0, 69, 70, 71, 72, 73, 74, 75, 100, 100.1),
+        cellheight = 12, cellwidth = 12,
+         #gaps_row = gapsrow,
+         #gaps_col = gapscol, 
+         display_numbers = T,
+         fontsize_number = 3,
+        filename="heatmap.rawseqdist.pdf", 
+        #gaps_row=9,
+        # color=colors,
+         #breaks=seq(range.min, range.max * 1, 0.5),
+         #legend_breaks=seq(range.min, range.max, 2),
+         #legend_labels=seq(range.min, range.max, 2),
+         legend_labels = seq(10,100,10)
+         )
+
+
+
